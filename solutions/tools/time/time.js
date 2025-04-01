cc7e7704f7c491fb2f62b5bce245b8ed442e45a7
BesKovOr <biojatek@gmail.com>
2025-04-01 20:36:08 +0200

    Let's try this

--- solutions/tools/time/time.js
+++ solutions/tools/time/time.js
@@ -347,7 +347,7 @@ function checkAnswers() {
                     } else if (month === "Február") {
                         expectedAnswer = "28/29";
                         isCorrect = input.value === "28" || input.value === "29" || input.value === "28/29";
-                    } else {
+                    } else if (["Február", "Április", "Június", "Szeptember", "November"].includes(month)){
                         expectedAnswer = "30";
                         isCorrect = input.value === "30";
                     }
