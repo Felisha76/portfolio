8575cccffa2a3c57439cf8559654ed2e2ac40fa1
BesKovOr <biojatek@gmail.com>
2025-04-01 20:38:00 +0200

    nope

--- solutions/tools/time/time.js
+++ solutions/tools/time/time.js
@@ -347,7 +347,7 @@ function checkAnswers() {
                     } else if (month === "Február") {
                         expectedAnswer = "28/29";
                         isCorrect = input.value === "28" || input.value === "29" || input.value === "28/29";
-                    } else if (["Február", "Április", "Június", "Szeptember", "November"].includes(month)){
+                    } else {
                         expectedAnswer = "30";
                         isCorrect = input.value === "30";
                     }
