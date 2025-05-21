a81954613662f0536e309b0cb446c5ce59b3dd31
Felisha76 <felisha19761228@gmail.com>
2025-05-21 12:38:22 +0200

    correcting typos

--- solutions/templates/digital_clock/digital_clock.js
+++ solutions/templates/digital_clock/digital_clock.js
@@ -6,11 +6,10 @@ function realtimeClock() {
 
     var amPm = (hours >= 12) ? "PM" : "AM";
     hours = (hours > 12) ? hours - 12 : hours; 
-    
     hours = ("0" + hours).slice(-2);
     minutes = ("0" + minutes).slice(-2);
     seconds = ("0" + seconds).slice(-2);
 
     document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds + " " + amPm;
-    var t =setTimeout(realtimeClock, 500);
+    setTimeout(realtimeClock, 500);
 }
